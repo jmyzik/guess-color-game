7ddc587b1b2d99c80369b70064db466b25c30b89
Janusz Myzik <janusz.myzik@gmail.com>
2020-09-03 23:28:32 +0200

    Updates

---
+++ precache-manifest.6eaf6ee5b8d2f331eb95019f29470dc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0124bd873a4e7adfefc1931bb7f3aae1",
+    "url": "/guess-color-name/index.html"
+  },
+  {
+    "revision": "0e60ad5a84f1360967fe",
+    "url": "/guess-color-name/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "bc992e8832e39abc6b4c",
+    "url": "/guess-color-name/static/js/2.d0fd6ea0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/guess-color-name/static/js/2.d0fd6ea0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e60ad5a84f1360967fe",
+    "url": "/guess-color-name/static/js/main.c7a380f0.chunk.js"
+  },
+  {
+    "revision": "cc85a3e51dd0f6e0b35d",
+    "url": "/guess-color-name/static/js/runtime-main.d2e82806.js"
+  }
+]);
